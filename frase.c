024834b471a2e7397949f722d6fc4cbcd4232af1
GustavoMartinsPaz <gusttavopaz18@gmail.com>
2026-03-08 15:41:42 -0300

    teste 6

---
+++ frase.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Vai Corinthians!\n");
+    return 0;
+}
---
+++ soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int soma;
+
+    soma = a + b;
+
+    printf("Resultado: %d\n", soma);
+
+    return 0;
+}
